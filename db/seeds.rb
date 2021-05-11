7d5ff4685074791d52f4535e87868b6b62ccc002
Amelia Haba <amelia.haba347@gmail.com>
2021-05-10 20:28:06 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+(1..10).each do |id|
+    Student.create!(
+        name: Faker::Name.name,
+        username: Faker::TvShows::TwinPeaks.character,
+        password: "Password",
+        age: rand(3..72),
+        email: Faker::Internet.email,
+        phone_number: Faker::PhoneNumber.phone_number
+   
+    )
+end

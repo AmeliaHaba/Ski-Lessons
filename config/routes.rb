2fcdcd64fa3fb9b31ad62245c0e0787d05b87521
EliasAHH <eliasiscoding@gmail.com>
2021-05-21 14:00:05 -0700

    omniauth shoudl be working

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/:anything', to: "application#wrong_page"
+  # work bitch.
   match '/auth/:provider/callback' => 'sessions#google', via: [:get,:post]
 end

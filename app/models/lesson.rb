40b81bf9a0188a1bbcd6724d24c7d0f0bc780dbe
Amelia Haba <amelia.haba347@gmail.com>
2021-05-28 12:35:56 -0400

    add another scope method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,9 +4,11 @@ class Lesson < ApplicationRecord
 
     validates :appointment_day, presence: true
     validates :level, presence: true 
+    
 
     scope :beginner_level, -> {where(level: "beginner")}
     scope :intermediate_level, -> {where(level: "intermediate")}
     scope :advanced_level, -> {where(level: "advanced")}
+
     
 end
